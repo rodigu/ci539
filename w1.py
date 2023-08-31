be954b58d4c98e8e3c646f3109a4f474a1bcc72d
Rodrigo Morais <rmoraisu@protonmail.com>
2023-08-31 10:49:32 -0500

    fixes to avg

--- w1.py
+++ w1.py
@@ -17,25 +17,20 @@ def get_avg_assignment(sample: pd.DataFrame):
     df['start_time'] = pd.to_timedelta(df['start_time'].apply(convert_time))
     df['end_time'] = pd.to_timedelta(df['end_time'].apply(convert_time))
 
-    df['total_time'] = df['end_time'] - df['start_time']
+    df.loc[df['total_time'], 'total_time'] = df['end_time'] - df['start_time']
 
     # Drop sections where start time is greater than end time
     df = df[df['start_time'] < df['end_time']]
 
+    # Drop unused columns
     df = df.drop(columns=['user_id', 'problem_id',
-                 'assistment_id', 'hint_count', 'original', 'correct'])
+                 'assistment_id', 'hint_count', 'original', 'correct', 'skill', 'problem_type'])
 
     # Average
     df = df.groupby('assignment_id').mean(numeric_only=False)
 
-    def td_to_str(td: pd.Timedelta):
-        s = str(td)
-        s = s.split()[2]
-        s.split(':')
-        return
-
     # Turn timedelta back to string
-    df['total_time'] = df['total_time'].apply(
-        str).apply(lambda s: s.split()[2])
+    df.loc[df['total_time'], 'total_time'] = df['total_time'].apply(
+        lambda s: s.seconds)
 
     return df
